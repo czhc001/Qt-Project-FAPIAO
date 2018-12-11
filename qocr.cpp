ba458aaa33da72826edf55e60457ab7d1749f4b1
czhc001 <1004385883@qq.com>
2018-12-11 21:14:38 +0800

    speed

--- qocr.cpp
+++ qocr.cpp
@@ -34,7 +34,7 @@ void QOcr::message(string sum1,string sum2, Mat image,bool flag){
     //std::cout << "!!! " << sum1 << " " << sum2 << std::endl;
     QString m1 = QString::fromStdString(sum1);
     QString m2 = QString::fromStdString(sum2);
-    //qDebug() << QString("!!!") << m1 << QString("!!!") <<m2;
+    //qDebug() << QString("n1: ") << m1 << QString("   n2: ") <<m2;
     //qDebug() << "QOcr emited";
 
     QImage img = MatToQImage(image);
